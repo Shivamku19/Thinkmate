a46633436b8266c7377873229a9bedfba1170553
Shivamkr-cod <namkeenshivam156@gmail.com>
2026-07-24 16:13:14 +0530

    completed

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -150,7 +150,7 @@ const Sidebar = ({isMenuOpen,setIsMenuOpen}) => {
   <div className="flex flex-col text-sm">
     <p>Credits: {user?.credits}</p>
     <p className="text-xs text-gray-400">
-      Purchase credits to use quickgpt
+      Purchase credits to use Thinkmate
     </p>
   </div>
 </div>
