f336e10353fca69653af6705615f79e399761c64
Shivamkr-cod <namkeenshivam156@gmail.com>
2026-07-27 03:26:01 +0530

    updated refresh issue

--- client/src/pages/Loading.jsx
+++ client/src/pages/Loading.jsx
@@ -16,8 +16,8 @@ const Loading = () => {
   }, []);
 
   return (
-    <div className="bg-linear-to-b from-[#531B81] to-[#29184B] backdrop-opacity-60 flex items-center justify-center h-screen w-screen text-white text-2xl">
-      <div className="w-10 h-10 rounded-full border-3 border-white border-t-transparent animate-spin"></div>
+    <div className="bg-gray-50 dark:bg-linear-to-b dark:from-[#242124] dark:to-[#000000] flex items-center justify-center h-screen w-screen text-emerald-600 dark:text-white text-2xl">
+      <div className="w-10 h-10 rounded-full border-3 border-emerald-600 dark:border-white border-t-transparent dark:border-t-transparent animate-spin"></div>
     </div>
   );
 };
